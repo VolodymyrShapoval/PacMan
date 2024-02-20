75ee63a2ff89f6c022e83fc2e112b73dfc597670
VolodymyrS <114925675+VolodymyrShapoval@users.noreply.github.com>
2024-02-20 22:21:34 +0200

    add maze and pacman

---
+++ maze.py
@@ -0,0 +1,23 @@
+# Map representing the maze
+maze_map = [
+    "xxxxxxxxxxxxxxxxxxxx",
+    "x        x         x",
+    "x xx xxx x x xxxx xx",
+    "x xx x   xx   x x  x",
+    "x x    x    x   x  x",
+    "x x xx   x  xxxx   x",
+    "x   xxx xxx xxx xx x",
+    "x x xx   x  xx  xx x",
+    "x      x    x  xxx x",
+    "xxx xx xxxx   xx x x",
+    "x x xx    x x      x",
+    "x x xx xx x xxxx xxx",
+    "x x xx      x    xxx",
+    "x      xxxx x xx   x",
+    "x xxxx x      xx  xx",
+    "x  x     xxxx  x   x",
+    "xx x x x xxxx    x x",
+    "xx xxx x   xxx xxx x",
+    "x      x x         x",
+    "xxxxxxxxxxxxxxxxxxxx"
+]
---
+++ pacman.py
@@ -0,0 +1,27 @@
+import pygame
+
+# Розмір кубіка пакмана
+CELL_SIZE = 15
+
+# Колір пакмана
+YELLOW = (255, 255, 0)
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+class Pacman(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x * CELL_SIZE, y * CELL_SIZE)
+        self.direction = LEFT
+
+    def update(self, walls):
+        self.rect.move_ip(self.direction[0] * CELL_SIZE, self.direction[1] * CELL_SIZE)
+        if pygame.sprite.spritecollide(self, walls, False):
+            self.rect.move_ip(-self.direction[0] * CELL_SIZE, -self.direction[1] * CELL_SIZE)
+            self.direction = (0, 0)

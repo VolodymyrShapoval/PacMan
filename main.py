a766d17178434e1856dd73fa57476f7e825e28d7
VolodymyrS <114925675+VolodymyrShapoval@users.noreply.github.com>
2024-02-21 18:52:44 +0200

    add new icon and background music

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ WIDTH, HEIGHT = 600, 600
 CELL_SIZE = 30
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pacman")
+icon = pygame.image.load("img/Pac_man_logo01.png")
+pygame.display.set_icon(icon)
+background_sound = pygame.mixer.Sound("sounds/background_music.mp3")
+background_sound.set_volume(0.15)
+background_sound.play()
+
 
 # Задній фон
 BLACK = (0, 0, 0)

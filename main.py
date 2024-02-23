a891515353550f571fa72445ca777ae46902d0c0
unknown <vgriss@knu.ua>
2024-02-23 13:45:03 +0200

    Pause

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 import sys
+import random
 from wall import *
 from pacman import Pacman
+
 
 # Initialize Pygame
 pygame.init()
@@ -15,9 +17,9 @@ pygame.display.set_caption("Pacman")
 icon = pygame.image.load("img/Pac_man_logo01.png")
 pygame.display.set_icon(icon)
 
-background_sound = pygame.mixer.Sound("sounds/background_music.mp3")
+background_sound = pygame.mixer.Sound("sounds/pacman_chomp.wav")
 background_sound.set_volume(0.15)
-background_sound.play()
+background_sound.play(loops=-1)
 
 # Задній фон
 WHITE = (255, 255, 255)
@@ -28,7 +30,7 @@ def main():
     all_sprites, walls, fruits = draw_maze()
 
     #Створення пакмана    
-    pacman = Pacman(60, 60, speed=5)  # Начальная позиция пакмана в пикселях
+    pacman = Pacman(40, 40, speed=5)  # Начальная позиция пакмана в пикселях
     all_sprites.add(pacman)
 
     #Таблиця рахунку
@@ -40,50 +42,67 @@ def main():
 
     clock = pygame.time.Clock()
 
+    paused = False  # Переменная для отслеживания состояния паузы
+    pause_text = font.render("Paused", True, WHITE)
+    last_pause_time = 0  # Время последней паузы
+
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    paused = not paused
+                    if paused:
+                        background_sound.stop() 
+                        last_pause_time = pygame.time.get_ticks()  
+                    else:
+                        background_sound.play(loops=-1)  
+                        last_fruit_spawn_time += pygame.time.get_ticks() - last_pause_time  
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            pacman.direction = (-1, 0)
-        elif keys[pygame.K_RIGHT]:
-            pacman.direction = (1, 0)
-        elif keys[pygame.K_UP]:
-            pacman.direction = (0, -1)
-        elif keys[pygame.K_DOWN]:
-            pacman.direction = (0, 1)
+        if not paused:  
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                pacman.direction = (-1, 0)
+            elif keys[pygame.K_RIGHT]:
+                pacman.direction = (1, 0)
+            elif keys[pygame.K_UP]:
+                pacman.direction = (0, -1)
+            elif keys[pygame.K_DOWN]:
+                pacman.direction = (0, 1)
 
-            # Check if it's time to spawn a new fruit
-        current_time = pygame.time.get_ticks()
-        if current_time - last_fruit_spawn_time >= fruit_spawn_interval:
-            last_fruit_spawn_time = current_time
-            if not fruits:  # Spawn a fruit only if there are none on the field
-                possible_spawns = [(x, y) for y, row in enumerate(maze_map) for x, char in enumerate(row) if char == " "]
-                if possible_spawns:
-                    x, y = random.choice(possible_spawns)
-                    fruit = Fruit(x, y)
-                    all_sprites.add(fruit)
-                    fruits.add(fruit)
 
-        # Check collision with fruits
-        fruit_collisions = pygame.sprite.spritecollide(pacman, fruits, True)
-        score += len(fruit_collisions)
+            current_time = pygame.time.get_ticks()
+            if current_time - last_fruit_spawn_time >= fruit_spawn_interval:
+                last_fruit_spawn_time = current_time
+                if not fruits:  
+                    possible_spawns = [(x, y) for y, row in enumerate(maze_map) for x, char in enumerate(row) if char == " "]
+                    if possible_spawns:
+                        x, y = random.choice(possible_spawns)
+                        fruit = Fruit(x, y)
+                        all_sprites.add(fruit)
+                        fruits.add(fruit)
 
-        # Update text
-        score_text = font.render(f"Score: {score}", True, WHITE)
 
-        pacman.update(walls)
+            fruit_collisions = pygame.sprite.spritecollide(pacman, fruits, True)
+            score += len(fruit_collisions)
+
+            pacman.update(walls)
+
 
         WIN.fill(BLACK)
         all_sprites.draw(WIN)
+        score_text = font.render(f"Score: {score}", True, WHITE)
         WIN.blit(score_text, (WIDTH - score_text.get_width() - 10, 10))
+
+        if paused:  
+            WIN.blit(pause_text, (WIDTH // 2 - pause_text.get_width() // 2, HEIGHT // 2 - pause_text.get_height() // 2))
+
         pygame.display.flip()
 
-
-        clock.tick(48)
+        clock.tick(30)
 
     pygame.quit()
     sys.exit()
